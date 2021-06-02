478504f1071d2792838113570b8c5e09175804f8
Eric Spichiger <e.spichiger@hiveweb.ch>
2021-06-02 16:21:29 +0200

    Init project

---
+++ telemaque-cf7.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Telemaque for Contact form 7
+Plugin URI: https://contactform7.com/
+Description: Just another contact form plugin. Simple but flexible.
+Author: Hiveweb : Eric Spichiger
+Author URI: https://hiveweb.ch/
+Text Domain: telemaque-cf7
+Domain Path: /languages/
+Version: 0.0.1
+*/
